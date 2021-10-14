cdc9a1f0e3895b3d3349fe2b918e4118ed0fd520
Snowager <59709492+Snowager@users.noreply.github.com>
2021-10-14 09:53:48 -0600

    Update script2.js

--- bacs200/test/script2.js
+++ bacs200/test/script2.js
@@ -1,7 +1,7 @@
 var degree = 0;
 $(window).on('swipeleft', function(event) {
     var durationC = 200;
-    $('.cube').animate({degree: '+=90'}, {
+    $('.cube').animate({degree: '-=90'}, {
         duration: durationC,
         step: function(degree,fx) {
         $(this).css('-webkit-transform', 'rotateY('+degree+'deg)');
@@ -11,7 +11,7 @@ $(window).on('swipeleft', function(event) {
 });
 $(window).on('swiperight', function(event) {
     var durationC = 200;
-    $('.cube').animate({degree: '-=90'}, {
+    $('.cube').animate({degree: '+=90'}, {
         duration: durationC,
         step: function(degree,fx) {
         $(this).css('-webkit-transform', 'rotateY('+degree+'deg)');
